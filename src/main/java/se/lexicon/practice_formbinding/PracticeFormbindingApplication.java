c98f209dfb5923914aa9d4ca02037b32c5d0deb0
asha joseph <ashajo183@gmail.com>
2021-04-23 12:12:46 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/practice_formbinding/PracticeFormbindingApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.practice_formbinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeFormbindingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeFormbindingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/practice_formbinding/PracticeFormbindingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.practice_formbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeFormbindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
